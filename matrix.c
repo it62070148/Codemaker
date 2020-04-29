05f7aae278fafb22039cae8dd16e3af00becfdea
it62070148 <54875111+it62070148@users.noreply.github.com>
2020-04-30 00:17:01 +0700

    Matrix Encrypt

--- matrix.c
+++ matrix.c
@@ -3,56 +3,45 @@
 #include "math.h"
 #include "ctype.h"
 int main(){
-  char massage[100], code[10], cha;
-  int c, matrix[3][3], count = 0, ind = 0, indxm = 0;
+  char massage[100], cha;
+  int c, matrix[3][3], count = 0, ind = 0, code[3][3]
+  , len;
   printf("Please input massage\n");
   scanf("%s", massage);
   printf("input code\n");
-  scanf("%s", code);
+  len = strlen(massage);
+  if (len%3 != 0){
+    for(int i = 1;i<=len%3;i++){
+      strcat(massage, "0");
+      len++;
+    }
+  }
+  int matrixmass[len/3][3], ans[len], sum = 0, a = 0;
+  for(int i = 0;i< len/3;i++){
+    for(int j = 0;j < 3;j++){
+    if (massage[ind] == '0'){
+      matrixmass[i][j] = 0;
+    }
+    else{
+    matrixmass[i][j] = massage[ind];}
+   ind++;}
+  }
   for (int i = 0;i < 3;i++){
-    for(int j =0;j<3;j++){
-      c = code[indxm];
-      cha = c-48;
-      matrix[i][j] = cha;
-      indxm++;
-    }
-    
-  }
-  int len = strlen((massage));
-  if(len%3 == 0){
-    len--;
-  }
-  struct test{
-    int keep[3];
-  }masstrix[len/3], decoder[len/3];
-  for(int i = 0;i < strlen(massage);i++){
-    c = massage[i];
-    masstrix[count].keep[ind] = c;
-    ind++;
-    if(i%3 == 2 && i!= 0){
-      count++;
-      ind = 0;
+    for(int j = 0;j< 3;j++){
+    scanf("%d", &code[i][j]);
     }
   }
-  int row = 0, col = 0, sum = 0, a=0, b = 0;
-  for(int i =0;i <= len/3;i++){
-    for(int k =0;k <3;k++){
-      for(int j =0;j<3;j++){
-      ind = masstrix[i].keep[j];
-      sum += ((ind*matrix[j][k]));
-      }
-      decoder[i].keep[k]= sum;
-      printf("%d", sum);
+  for (int i =0;i< len/3;i++){
+     for(int k = 0;k < 3;k++){
+       for(int j = 0;j < 3;j++){
+         sum = sum + (matrixmass[i][j]*code[j][k]);
+       }
+      ans[a] = sum;
       sum = 0;
-    }
-    printf("\n");
+      a++;
+     }
   }
-  for(int i =0; i<=len/3;i++){
-    for(int j = 0;j<3;j++){
-      printf("%d ", decoder[i].keep[j]);
+  for(int i =0; i<a;i++){
+      printf("%d ", ans[i]);
     }
-    printf("\n");
-  }
-  
-  
 }
