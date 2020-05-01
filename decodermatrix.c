c1c5d0d02e98fdd1d67b27f96dc8b0bd98155b76
it62070148 <54875111+it62070148@users.noreply.github.com>
2020-05-02 02:17:31 +0700

    Update decodermatrix.c

--- decodermatrix.c
+++ decodermatrix.c
@@ -4,13 +4,14 @@
 
 int transintmirror(char c, int ind);
 int transint(int sum, int ind);
-float determinant(float [][50], float);
-void cofactor(float [][50], float, int [], int);
-void transpose(float [][50], float [][50], float, int [], int);
+float determinant(float [][3], float);
+void cofactor(float [][3], float, int [], int);
+void transpose(float [][3], float [][3], float, int [], int);
 int main(){
-  float a[50][50], k = 3, d;
+  float a[3][3], d, k = 3;
   char code[100], c, word[100];
-  int i, j, sum = 0, ind = 0, massint[100], l = 0, inverse[50][50];
+  int i, j, sum = 0, ind = 0, massint[100], l = 0, inverse[3][3]
+  ;
   scanf("%[^\n]", code);
   strcat(code, " ");
   printf("Enter the elements of %.0fX%.0f Matrix : \n", k, k);
@@ -18,6 +19,11 @@ int main(){
      for (j = 0;j < k; j++){
         scanf("%f", &a[i][j]);
         }
+    }
+  for (i = 0;i < k; i++){
+    for (j = 0;j < k; j++){
+      printf("%f ", a[i][j]);
+      }
     }
   // printf("%ld\n", strlen(code));
   for (i = 0;i < strlen(code);i++){
@@ -34,10 +40,6 @@ int main(){
       l++;
     }
   }
-  printf("%d\n", l);
-  for (i = 0;i < l;i++){
-    printf("%d ", massint[i]);
-  }
   d = determinant(a, k);
   if (d == 0){
    printf("\nInverse of Entered Matrix is not possible\n");
@@ -48,35 +50,50 @@ int main(){
 }
  
 /*For calculating Determinant of the Matrix */
-float determinant(float a[50][50], float k){
-  float s = 1, det = 0, b[50][50];
+float determinant(float a[3][3], float k)
+{
+  float s = 1, det = 0, b[3][3];
   int i, j, m, n, c;
-    for (c = 0; c < k; c++){
-    m = 0;
-    n = 0;
-    for (i = 0;i < k; i++){
-        for (j = 0 ;j < k; j++){
-            b[i][j] = 0;
-            if (i != 0 && j != c){
-                b[m][n] = a[i][j];
-                if (n < (k - 2))
-                n++;
-                else{
-                    n = 0;
-                    m++;
-                        }
-                }
-            }
-        }
-        det = det + s * (a[0][c] * determinant(b, k - 1));
-        s = -1 * s;
-        }
+  if (k == 1)
+    {
+     return (a[0][0]);
+    }
+  else
+    {
+     det = 0;
+     for (c = 0; c < k; c++)
+       {
+        m = 0;
+        n = 0;
+        for (i = 0;i < k; i++)
+          {
+            for (j = 0 ;j < k; j++)
+              {
+                b[i][j] = 0;
+                if (i != 0 && j != c)
+                 {
+                   b[m][n] = a[i][j];
+                   if (n < (k - 2))
+                    n++;
+                   else
+                    {
+                     n = 0;
+                     m++;
+                     }
+                   }
+               }
+             }
+          det = det + s * (a[0][c] * determinant(b, k - 1));
+          s = -1 * s;
+          }
+    }
+ 
     return (det);
 }
  
-void cofactor(float num[50][50], float f, int massint[100], int l){
- float b[50][50], fac[50][50];
- int p, q, m, n, i, j, inverse[50][50];
+void cofactor(float num[3][3], float f, int massint[100], int l){
+ float b[3][3], fac[3][3];
+ int p, q, m, n, i, j, inverse[3][3];
  for (q = 0;q < f; q++){
    for (p = 0;p < f; p++){
      m = 0;
@@ -100,9 +117,9 @@ void cofactor(float num[50][50], float f, int massint[100], int l){
   transpose(num, fac, f, massint, l);
 }
 /*Finding transpose of matrix*/ 
-void transpose(float num[50][50], float fac[50][50], float r, int massint[100], int l){
-  int i, j, k, skip = 0, a = 0;
-  float b[50][50], inverse[50][50], d, sum = 0, ans[100];
+void transpose(float num[3][3], float fac[3][3], float r, int massint[100], int l){
+  int i, j, k, skip = 0, a = 0, c;
+  float b[3][3], inverse[3][3], d, sum = 0, ans[100];
   for (i = 0;i < r; i++){
      for (j = 0;j < r; j++){
          b[i][j] = fac[j][i];
@@ -128,7 +145,8 @@ void transpose(float num[50][50], float fac[50][50], float r, int massint[100], 
     skip += 3;
   } 
   for(i = 0;i< a;++i){
-    _printf("%c", ans[i]);
+    c = ans[i];
+    printf("%c", c);
   }
    
 }
